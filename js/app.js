12ddc4212c5d927b093b4cedee2c6efb4e53ca09
Andy <andrew_grant@live.com>
2016-01-18 09:53:59 +1000

    Fixed tab spacing on code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 $(document).foundation({
-    equalizer: {
-        equalize_on_stack: true
-    }
+	equalizer: {
+		equalize_on_stack: true
+	}
 });
 
 $(document).ready(function(){
-  $('.slick-slider').slick({
-    autoplay: true,
+	$('.slick-slider').slick({
+		autoplay: true,
 		autoplaySpeed: 4000,
 		dots: false,
 		pauseOnHover: true,
@@ -14,7 +14,7 @@ $(document).ready(function(){
 		appendArrows: $(".slick-arrow-box"),
 		prevArrow: "<i class='fa fa-arrow-left'></i>",
 		nextArrow: "<i class='fa fa-arrow-right'></i>"
-  });
+	});
 	
 	// Causes the model-search-tip to appear when a user selects a Make
 	$('.make-search-element > select' ).bind('change', function() {
